274d7a3b03756db097d37a9699a188b1e19e393e
Bruno <iam@corrni.io>
2018-09-11 19:04:08 +0200

    Getting started with Dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
-import { StockInterface, StocksService } from './services/stocks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  stocks: Array<StockInterface>;
-
-  constructor(service: StocksService) {
-    service.load(['GOOG']).subscribe(stocks => {
-      console.log(stocks);
-      this.stocks = stocks;
-    });
-  }
-}
+export class AppComponent { }
